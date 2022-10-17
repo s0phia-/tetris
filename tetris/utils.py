65360fc36153905f6c43faf922b555d1cc811941
Sophia <sophiajones@live.co.uk>
2022-10-17 17:16:30 +0200

    Untangled environment from agent

--- tetris/utils.py
+++ tetris/utils.py
@@ -1,21 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-from numba import njit
-# import telegram
-from os.path import expanduser
-home = expanduser("~")
-
-
-# def notify_ending(message):
-#     print("NOTIFY ENDING is DEPRECATED!")
-#     with open(os.path.join(home, "tel_keys.json"), 'r') as keys_file:
-#         k = json.load(keys_file)
-#         token = k['telegram_token']
-#         chat_id = k['telegram_chat_id']
-#
-#     bot = telegram.Bot(token=token)
-#     bot.sendMessage(chat_id=chat_id, text=message)
+# from numba import njit
 
 
 class Bunch(object):
@@ -23,66 +9,21 @@ class Bunch(object):
         self.__dict__.update(adict)
 
 
-def print_board_to_string(state):
-    string = "\n"
-    for row_ix in range(state.representation.shape[0]):
-        # Start from top
-        row_ix = state.num_rows - row_ix - 1
-        string += "|"
-        for col_ix in range(state.num_columns):
-            if state.representation[row_ix, col_ix]:
-                string += "██"
-            else:
-                string += "  "
-        string += "|\n"
-    return string
-
-
-def print_tetromino(tetromino_index):
-    if tetromino_index == 0:
-        string = '''
-██ ██ ██ ██'''
-    elif tetromino_index == 1:
-        string = '''
-██ ██ 
-██ ██'''
-    elif tetromino_index == 2:
-        string = '''
-   ██ ██ 
-██ ██'''
-    elif tetromino_index == 3:
-        string ='''
-██ ██ 
-   ██ ██'''
-    elif tetromino_index == 4:
-        string ='''
-   ██
-██ ██ ██'''
-    elif tetromino_index == 5:
-        string ='''
-██ ██ ██
-██'''
-    elif tetromino_index == 6:
-        string="""
-██ ██ ██
-      ██"""
-    return string
-
-@njit(cache=False)
+# @njit
 def one_hot_vector(one_index, length):
     out = np.zeros(length)
     out[one_index] = 1.
     return out
 
 
-@njit(cache=False)
+# @njit
 def vert_one_hot(one_index, length):
     out = np.zeros((length, 1))
     out[one_index] = 1.
     return out
 
 
-@njit(cache=False)
+# @njit
 def compute_action_probabilities(action_features, weights, temperature):
     utilities = action_features.dot(weights) / temperature
     utilities = utilities - np.max(utilities)
@@ -91,83 +32,33 @@ def compute_action_probabilities(action_features, weights, temperature):
     return probabilities
 
 
-@njit(cache=False)
+# @njit
 def grad_of_log_action_probabilities(features, probabilities, action_index):
     features_of_chosen_action = features[action_index]
     grad = features_of_chosen_action - features.T.dot(probabilities)
     return grad
 
 
-@njit(cache=False)
+#  @njit
 def softmax(U):
     ps = np.exp(U - np.max(U))
     ps /= np.sum(ps)
     return ps
 
 
-def plot_multiple_learning_curves(plots_path, compare_results, compare_ids, x_axis, title=""):
-    # Plot and save learning curves.
-    fig1, ax1 = plt.subplots()
-    for test_results_ix in range(len(compare_results)):
-        test_results = compare_results[test_results_ix]
-        mean_array = np.mean(test_results, axis=(0, 2))
-        serr_array = np.std(test_results, axis=(0, 2)) / np.sqrt(compare_results[0].shape[0])
-        ax1.plot(x_axis, mean_array, label=compare_ids[test_results_ix])
-        ax1.fill_between(x_axis, mean_array - serr_array, mean_array + serr_array, alpha=0.2)
-
-    plt.title(f'Mean performance{title}')
-    plt.xlabel('Iteration')
-    plt.ylabel('Mean score')
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
-    plt.show()
-    fig1.savefig(os.path.join(plots_path, "mean_performance"), bbox_inches='tight')
-    plt.close()
-
-    fig1, ax1 = plt.subplots()
-    for test_results_ix in range(len(compare_results)):
-        test_results = compare_results[test_results_ix]
-        mean_array = np.median(test_results, axis=(0, 2))
-        ax1.plot(x_axis, mean_array, label=compare_ids[test_results_ix])
-
-    plt.title(f'Median performance{title}')
-    plt.xlabel('Iteration')
-    plt.ylabel('Median score')
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
-    plt.show()
-    fig1.savefig(os.path.join(plots_path, "median_performance"), bbox_inches='tight')
-    plt.close()
-
-    fig1, ax1 = plt.subplots()
-    for test_results_ix in range(len(compare_results)):
-        test_results = compare_results[test_results_ix]
-        mean_array = np.max(test_results, axis=(0, 2))
-        ax1.plot(x_axis, mean_array, label=compare_ids[test_results_ix])
-
-    plt.title(f'Max performance{title}')
-    plt.xlabel('Iteration')
-    plt.ylabel('Max score')
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
-    plt.show()
-    fig1.savefig(os.path.join(plots_path, "max_performance"), bbox_inches='tight')
-    plt.close()
-
-
-def plot_learning_curve(plots_path, test_results, x_axis, suffix=""):
-
+def plot_learning_curve(plots_path, test_results, x_axis):
     mean_array = np.mean(test_results, axis=(0, 2))
     median_array = np.median(test_results, axis=(0, 2))
     max_array = np.max(test_results, axis=(0, 2))
-    serr_array = np.std(test_results, axis=(0, 2)) / np.sqrt(test_results[0].shape[0])
 
 
     # Plot and save learning curves.
     fig1, ax1 = plt.subplots()
     ax1.plot(x_axis, mean_array, label="mean")
-    # ax1.plot(x_axis, median_array, label="median")
-    ax1.fill_between(x_axis, mean_array - serr_array, mean_array + serr_array, alpha=0.2)
+    ax1.plot(x_axis, median_array, label="median")
     plt.legend()
     fig1.show()
-    fig1.savefig(os.path.join(plots_path, "mean_performance" + suffix))
+    fig1.savefig(os.path.join(plots_path, "mean_performance"))
     plt.close()
 
     # Plot and save learning curves.
@@ -175,57 +66,35 @@ def plot_learning_curve(plots_path, test_results, x_axis, suffix=""):
     ax1.plot(x_axis, max_array, label="max")
     plt.legend()
     fig1.show()
-    fig1.savefig(os.path.join(plots_path, "max_performance"+ suffix))
+    fig1.savefig(os.path.join(plots_path, "max_performance"))
     plt.close()
 
 
-def plot_individual_agent(plots_path, tested_weights, test_results, agent_ix, x_axis):
+def plot_individual_agent(plots_path, tested_weights, test_results, weights_storage, agent_ix, x_axis):
     feature_names = ['rows_with_holes', 'column_transitions', 'holes', 'landing_height', 'cumulative_wells',
                      'row_transitions', 'eroded', 'hole_depth']
     # Compute tested_weight paths
-    if tested_weights is not None:
-        fig1, ax1 = plt.subplots()
-        for ix in range(tested_weights.shape[1]):
-            ax1.plot(x_axis, tested_weights[:, ix], label=feature_names[ix])
-        plt.legend()
-        fig1.show()
-        fig1.savefig(os.path.join(plots_path, "weight_paths_tested" + str(agent_ix)))
-        plt.close()
+    fig1, ax1 = plt.subplots()
+    for ix in range(tested_weights.shape[1]):
+        ax1.plot(x_axis, tested_weights[:, ix], label=feature_names[ix])
+    plt.legend()
+    fig1.show()
+    fig1.savefig(os.path.join(plots_path, "weight_paths_tested" + str(agent_ix)))
+    plt.close()
 
-    # # Compute weights_storage paths
-    # fig1, ax1 = plt.subplots()
-    # for ix in range(weights_storage.shape[1]):
-    #     ax1.plot(weights_storage[:, ix], label=feature_names[ix])
-    # plt.legend()
-    # fig1.show()
-    # fig1.savefig(os.path.join(plots_path, "weight_paths" + str(agent_ix)))
-    # plt.close()
-
-    # # Compute weight distances
-    # # tested_weights = np.random.normal(size=(4, 8))
-    # diff_weights = np.diff(tested_weights, axis=0)
-    # distances = np.sqrt(np.sum(diff_weights ** 2, axis=1))
-    # fig1, ax1 = plt.subplots()
-    # ax1.plot(distances, label="l2 distance to previous")
-    # plt.legend()
-    # fig1.show()
-    # fig1.savefig(os.path.join(plots_path, "distances" + str(agent_ix)))
-    # plt.close()
-
-    # # Compute RELATIVE weight distances
-    # relative_diff_weights = np.diff(tested_weights / np.abs(tested_weights[:, 0][:, np.newaxis]), axis=0)
-    # distances = np.sqrt(np.sum(relative_diff_weights ** 2, axis=1))
-    # fig1, ax1 = plt.subplots()
-    # ax1.plot(distances, label="l2 RELATIVE distance to previous")
-    # plt.legend()
-    # fig1.show()
-    # fig1.savefig(os.path.join(plots_path, "relative_distances" + str(agent_ix)))
-    # plt.close()
+    # Compute weights_storage paths
+    fig1, ax1 = plt.subplots()
+    for ix in range(weights_storage.shape[1]):
+        ax1.plot(weights_storage[:, ix], label=feature_names[ix])
+    plt.legend()
+    fig1.show()
+    fig1.savefig(os.path.join(plots_path, "weight_paths" + str(agent_ix)))
+    plt.close()
 
     # Compute learning curves (mean and median)
     mean_array = np.mean(test_results, axis=1)
     median_array = np.median(test_results, axis=1)
-    # max_array = np.max(test_results, axis=1)
+    max_array = np.max(test_results, axis=1)
 
     # Plot and save learning curves.
     fig1, ax1 = plt.subplots()
@@ -235,14 +104,6 @@ def plot_individual_agent(plots_path, tested_weights, test_results, agent_ix, x_
     fig1.show()
     fig1.savefig(os.path.join(plots_path, "mean_performance" + str(agent_ix)))
     plt.close()
-
-    # # Plot and save learning curves.
-    # fig1, ax1 = plt.subplots()
-    # ax1.plot(max_array, label="max")
-    # plt.legend()
-    # fig1.show()
-    # fig1.savefig(os.path.join(plots_path, "max_performance" + str(agent_ix)))
-    # plt.close()
 
 
 def plot_analysis(plots_path, tested_weights, test_results, weights_storage, agent_ix, x_axis):
@@ -309,4 +170,24 @@ def plot_analysis(plots_path, tested_weights, test_results, weights_storage, age
     fig1.savefig(os.path.join(plots_path, "max_performance" + str(agent_ix)))
     plt.close()
 
+
+
+
+
+# TODO
+
+
+def print_board_to_string(state):
+    string = "\n"
+    for row_ix in range(state.representation.shape[0]):
+        # Start from top
+        row_ix = state.num_rows - row_ix - 1
+        string += "|"
+        for col_ix in range(state.num_columns):
+            if state.representation[row_ix, col_ix]:
+                string += "██"
+            else:
+                string += "  "
+        string += "|\n"
+    return string
 
