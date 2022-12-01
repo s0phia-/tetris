eaea924fcb95c38c172e91aa41a5d1b24ddb309c
s0phia- <sophiajones@live.co.uk>
2022-12-01 11:07:29 +0100

    Remove unnecessary parameters

--- example_play.py
+++ example_play.py
@@ -3,18 +3,18 @@ import numpy as np
 
 feature_directions = np.array([-1, -1, -1, -1, -1, -1, 1, -1])
 
-env = Tetris(10, 10, False, feature_directions=feature_directions)
+env = Tetris(10, 10, feature_directions=feature_directions)
 env.reset()
 done = False
 cleared_lines = 0
 
 for x in range(100):
-    env.print_current_tetromino()  ##
+    #env.print_current_tetromino()  ##
     after_state_features = env.get_after_states()
     i = np.argmax([np.sum(y) for y in after_state_features])  # equal weights, directed
     print(after_state_features[i])
     observation, reward, done, _ = env.step(i)
-    env.print_current_board()  ##
+    env.render()  ##
     cleared_lines += reward
     # print(cleared_lines)  ##
     if done:
--- game.py
+++ game.py
@@ -5,25 +5,19 @@ from tetris.utils import print_board_to_string
 
 
 class Tetris:
-    def __init__(self, num_columns, num_rows, verbose=False, feature_directions=None,
-                 plot_intermediate_results=False, feature_type='bcts', num_features=8,
-                 tetromino_size=4, target_update=1, max_cleared_test_lines=np.inf):
+    def __init__(self, num_columns, num_rows, feature_directions=None,
+                 feature_type='bcts', num_features=8,
+                 tetromino_size=4):
+
+        # Saving parameters
         self.feature_directions = feature_directions
-        self.afterstates = None
         self.num_columns = num_columns
         self.num_rows = num_rows
-        self.tetromino_size = tetromino_size
-        # self.player = player
-        self.verbose = verbose
-        self.target_update = target_update
-        self.num_features = num_features
         self.feature_type = feature_type
-        self.n_fields = self.num_columns * self.num_rows
-        self.game_over = False
-        self.current_state = state.State(
-            representation=np.zeros((self.num_rows + self.tetromino_size, self.num_columns), dtype=np.int_),
-            lowest_free_rows=np.zeros(self.num_columns, dtype=np.int_), num_features=num_features,
-            feature_type=feature_type)
+        self.num_features = num_features
+        self.tetromino_size = tetromino_size
+
+        # The tetronimoes
         self.tetrominos = [tetromino.Straight(feature_type, num_features, self.num_columns),
                            tetromino.RCorner(feature_type, num_features, self.num_columns),
                            tetromino.LCorner(feature_type, num_features, self.num_columns),
@@ -31,63 +25,83 @@ class Tetris:
                            tetromino.SnakeR(feature_type, num_features, self.num_columns),
                            tetromino.SnakeL(feature_type, num_features, self.num_columns),
                            tetromino.T(feature_type, num_features, self.num_columns)]
-        self.tetromino_sampler = tetromino.TetrominoSamplerRandom(self.tetrominos)
-        self.cleared_lines = 0
-        self.state_samples = []
-        self.cumulative_steps = 0
-        self.max_cleared_test_lines = max_cleared_test_lines
-        self.plot_intermediate_results = plot_intermediate_results
-        self.current_tetromino = None
+
+        # game setup
+        self.tetromino_sampler = tetromino.TetrominoSampler(self.tetrominos)
+        self.reset()
 
     def reset(self):
-        self.game_over = False
+        # Reset state and choose first tetronimno
         self.current_state = state.State(
             representation=np.zeros((self.num_rows + self.tetromino_size, self.num_columns), dtype=np.int_),
             lowest_free_rows=np.zeros(self.num_columns, dtype=np.int_), num_features=self.num_features,
             feature_type=self.feature_type)
-        self.tetromino_sampler = tetromino.TetrominoSampler(self.tetrominos)
-        self.cleared_lines = 0
-        self.state_samples = []
-        self.current_tetromino = self.tetromino_sampler.next_tetromino()
-        self.afterstates = None
 
-    def get_after_states(self):
+        self.current_tetromino = self.tetromino_sampler.next_tetromino()
+
+        # return np.append(self.current_state.representation.flatten(), self.current_tetromino.tet_ind)
+
+    def get_after_states(self, include_terminal=False):
         afterstates = self.current_tetromino.get_after_states(self.current_state)  # the actions are afterstates
-        available_after_states = np.array([child for child in afterstates if not child.terminal_state])
-        if len(available_after_states) == 0:
-            self.game_over = True
-        num_states = len(available_after_states)
-        action_features = np.zeros((num_states, self.num_features))
-        for ix, after_state in enumerate(available_after_states):
+        self.afterstates = np.array([child for child in afterstates if not child.terminal_state])
+        action_features = np.zeros((len(self.afterstates), self.num_features))
+        for ix, after_state in enumerate(self.afterstates):
             action_features[ix] = after_state.get_features(direct_by=self.feature_directions)  # can use directions here
-        self.afterstates = available_after_states
-        return action_features
+        # todo
+        if include_terminal:
+            all_afterstates = np.zeros((len(afterstates), self.num_features))
+            for ix, after_state in enumerate(afterstates):
+                all_afterstates[ix] = after_state.get_features(direct_by=self.feature_directions)  # can use directions here
+            return action_features, all_afterstates
+        else:
+            return action_features
 
-    def step(self, action_ix):
-        observation_features = self.get_after_states()[action_ix]
-        observation = self.afterstates[action_ix]
-        self.cleared_lines += observation.n_cleared_lines
-        reward = observation.n_cleared_lines  # Malte used self.cleared_lines for this
-        self.current_state = observation
+    def step(self, action):
+        observation_features = self.get_after_states()[action]
+        self.current_state = self.afterstates[action]
+        reward = self.current_state.n_cleared_lines  # Malte used self.cleared_lines for this
         self.current_tetromino = self.tetromino_sampler.next_tetromino()
-        done = self.is_game_over(observation)
-        info = None
-        return observation_features, reward, done, info
+        done = self.is_game_over(self.current_state)
+        return observation_features, reward, done, None
 
     def is_game_over(self, state):
-        afterstates = self.current_tetromino.get_after_states(state)  # the actions are afterstates
+        afterstates = self.current_tetromino.get_after_states(state)
         available_after_states = np.array([child for child in afterstates if not child.terminal_state])
         if len(available_after_states) == 0:
-            self.game_over = True
             return True
         else:
             return False
 
-    def print_current_board(self):
-        print(print_board_to_string(self.current_state))
+    def get_best_policy(self):
 
-    def print_current_tetromino(self):
-        print(self.current_tetromino.__repr__())
+        after_states = self.current_tetromino.get_after_states(self.current_state)
+
+        after_state_fitness = np.array(list(map(self.fitness, after_states)))
+
+        best_policy = (after_state_fitness == after_state_fitness.max()).astype(float)
+        best_policy /= best_policy.sum()
+
+        return best_policy
+
+    def fitness(self, state):
+
+        state_features = state.get_features()
+
+        fitness_value = state_features[0] * -24.04 + \
+                        state_features[1] * -19.77 + \
+                        state_features[2] * -13.08 + \
+                        state_features[3] * -12.63 + \
+                        state_features[4] * -10.49 + \
+                        state_features[5] * -9.22 + \
+                        state_features[6] * 6.6 + \
+                        state_features[7] * -1.61
+
+        return fitness_value
+
+    def render(self):
+
+        print(print_board_to_string(self.current_state))
+        print(self.current_tetromino)
 
     def get_current_state_features(self):
         return self.current_state.get_features(direct_by=self.feature_directions)
